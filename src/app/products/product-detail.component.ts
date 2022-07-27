a42e90b83aaae0f3e8f0f2e7f827a65084eb92e7
Soumyak1303 <soumyak1303@gmail.com>
2022-07-27 23:23:09 +0530

    routing with params and code activation

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './products';
 
 @Component({
   //selector: 'pm-product-detail',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'product Details';
-  constructor() {}
+  product: IProduct | undefined;
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
